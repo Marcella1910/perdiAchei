4f3482ce638ec14a30bbfa3558b7d37b1772ad0d
vehzinha <101737211+vehzinha@users.noreply.github.com>
2024-06-06 12:48:43 -0300

    versão final do js do cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,68 +1,61 @@
+const botao = document.getElementById("btnCad");
+botao.addEventListener("click", validarForm);
+
+
+
 function validarForm() {
-  var mat = document.getElementById("mat").value.trim();
+
+
+  var nome = document.getElementById("nome").value.trim();
   var user = document.getElementById("user").value.trim();
   var email = document.getElementById("email").value.trim();
   var senha = document.getElementById("senha").value.trim();
   var cnfsenha = document.getElementById("cnfsenha").value.trim();
 
-  if (mat === "") {
-      mostraErro("mat", "Matrícula deve ser preenchida.");
-      return false;
-  } else {
-      removeErro("mat");
+  
+
+  if(nome === ""){
+    mostraErro("Nome deve ser preenchido");
+    return false;
   }
 
   if (user === "") {
-      mostraErro("user", "Nome de usuário deve ser preenchido.");
+      mostraErro("Nome de usuário deve ser preenchido.");
       return false;
-  } else {
-      removeErro("user");
   }
 
   if (email === "") {
-      mostraErro("email", "Email deve ser preenchido.");
+      mostraErro("Email deve ser preenchido.");
       return false;
-  } else {
-      removeErro("email");
   }
 
   if (senha === "") {
-      mostraErro("senha", "Senha deve ser preenchida.");
+      mostraErro("Senha deve ser preenchida.");
       return false;
   } else if (senha.length < 6 || senha.length > 30) {
-      mostraErro("senha", "Senha deve ter entre 6 e 30 caracteres.");
+      mostraErro("Senha deve ter entre 6 e 30 caracteres.");
       return false;
-  } else {
-      removeErro("senha");
   }
 
   if (cnfsenha === "") {
-      mostraErro("cnfsenha", "Confirme sua senha.");
+      mostraErro("Confirme sua senha.");
       return false;
   } else if (senha !== cnfsenha) {
-      mostraErro("cnfsenha", "As senhas não coincidem.");
+      mostraErro("As senhas não coincidem.");
       return false;
-  } else {
-      removeErro("cnfsenha");
-  }
+  } 
 
   // Se todas as validações passarem, redireciona para "feed.html"
   window.location.href = "feed.html";
   return false; // Para evitar que o formulário seja enviado antes do redirecionamento
 }
 
-function mostraErro(id, mensagem) {
-  var small = document.getElementById(id + "-error");
+function mostraErro(mensagem) {
+  var small = document.getElementById("erro");
   small.innerText = mensagem;
   small.style.visibility = "visible";
-  var input = document.getElementById(id);
-  input.classList.add("error");
+  
+  
 }
 
-function removeErro(id) {
-  var small = document.getElementById(id + "-error");
-  small.innerText = "";
-  small.style.visibility = "hidden";
-  var input = document.getElementById(id);
-  input.classList.remove("error");
-}
+
