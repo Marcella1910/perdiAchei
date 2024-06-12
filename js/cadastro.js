c7f7eac8b36ed682b655ed6411afb41e4b79c4fb
vehzinha <101737211+vehzinha@users.noreply.github.com>
2024-06-11 21:29:30 -0300

    melhoras na validação do cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,5 @@
 //recuperar dados do localStorage
+let nomes = JSON.parse(localStorage.getItem('nomes')) || [];
 let users = JSON.parse(localStorage.getItem('users')) || [];
 let emails = JSON.parse(localStorage.getItem('emails')) || [];
 let senhas = JSON.parse(localStorage.getItem('senhas')) || [];
@@ -28,10 +29,16 @@ function validarForm() {
   if (user === "") {
       mostraErro("Nome de usuário deve ser preenchido.");
       return false;
-  }
+  } else if (users.includes(user)) { //checa se o nome de usuário escolhido já é utilizado, verificando sua existência na lista
+      mostraErro("Nome de usuário já existe. Por favor, escolha outro.");
+      return false;
+}
 
   if (email === "") {
       mostraErro("Email deve ser preenchido.");
+      return false;
+  } else if (emails.includes(email)) { //checa se o email já é utilizado, verificando sua existência na lista
+      mostraErro("E-mail já vinculado a uma conta. Por favor, escolha outro.");
       return false;
   }
 
@@ -52,14 +59,23 @@ function validarForm() {
   } 
 
   // armazenando as informações em listas
+
+  nomes.push(nome);
   users.push(user);
   emails.push(email);
   senhas.push(senha);
 
   // armazenando no localStorage
+  localStorage.setItem('nomes', JSON.stringify(nomes));
   localStorage.setItem('users', JSON.stringify(users));
   localStorage.setItem('emails', JSON.stringify(emails));
   localStorage.setItem('senhas', JSON.stringify(senhas));
+
+  // Imprimindo as listas no console para facilitar acompanhamento
+  console.log("Nomes: ", nomes);
+  console.log("Users: ", users);
+  console.log("Emails: ", emails);
+  console.log("Senhas: ", senhas);
 
 
   // Se todas as validações passarem, redireciona para "feed.html"
@@ -71,8 +87,6 @@ function mostraErro(mensagem) {
   var small = document.getElementById("erro");
   small.innerText = mensagem;
   small.style.visibility = "visible";
-  
-  
 }
 
 
