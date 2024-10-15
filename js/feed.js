cab5e24ceee4a329e303d527aac1fb8961e9e9cb
briemodd <bibiairam1@gmail.com>
2024-10-15 19:36:02 -0300

    refzendo a pagina de meuperfil

--- js/feed.js
+++ js/feed.js
@@ -80,3 +80,47 @@ document.addEventListener('click', function(e) {
         });
     }
 });
+
+
+const activityButton = document.querySelector('.menu-item-notifications');
+const activityDropdown = document.querySelector('.notification-panel');
+
+activityButton.addEventListener('click', function(e) {
+    e.stopPropagation(); // Evita o fechamento imediato
+
+    if (activityDropdown.classList.contains('show')) {
+        // Se o painel está visível, oculta-o com uma animação de fade out
+        activityDropdown.style.opacity = '0'; // Inicia o fade-out
+        setTimeout(() => {
+            activityDropdown.classList.remove('show'); // Após a animação, oculta completamente
+        }, 300); // O tempo deve corresponder ao da transição definida no CSS (0.3s)
+    } else {
+        // Se o painel está oculto, exibe-o com fade-in
+        activityDropdown.classList.add('show'); // Mostra o painel
+        setTimeout(() => {
+            activityDropdown.style.opacity = '1'; // Aplica o fade-in
+        }, 10); // Pequeno atraso para garantir a transição de opacity
+    }
+});
+
+// Fecha o painel de notificações ao clicar fora dele
+document.addEventListener('click', function(e) {
+    if (!e.target.closest('.menu-item-notifications') && !e.target.closest('.notification-panel')) {
+        activityDropdown.style.opacity = '0'; // Inicia o fade-out
+        setTimeout(() => {
+            activityDropdown.classList.remove('show'); // Oculta o painel após o fade-out
+        }, 300); // O tempo deve coincidir com a duração da transição (0.3s)
+    }
+});
+
+
+// Seleciona todos os elementos clicáveis (foto de perfil, nome e username)
+const clickableElements = document.querySelectorAll('.clickable-profile');
+
+// Adiciona um evento de clique em cada um deles
+clickableElements.forEach(element => {
+    element.addEventListener('click', function() {
+        // Redireciona para a página meuperfil.html
+        window.location.href = 'meuperfil.html';
+    });
+});
