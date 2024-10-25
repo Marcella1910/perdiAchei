df33bae10b797c14c783320f8b33d1db144bb431
briemodd <bibiairam1@gmail.com>
2024-10-25 20:03:38 -0200

    consertando botao de cancelar

--- feed.php
+++ feed.php
@@ -574,7 +574,7 @@
                     <h3>Entrar em contato com @chuu</h3>
                     <p>Deseja reivindicar um item? Preencha as informações necessárias e sua mensagem será enviada por e-mail para o dono da postagem.</p>
                     <div class="bts-popup">
-                        <button class="cancel-button" onclick="closeConfirmPopup()">Cancelar</button>
+                        <button class="cancelarReport" onclick="closeConfirmPopup()">Cancelar</button>
                         <button class="confirm-button" onclick="openFormPopup()">Confirmar</button>
                     </div>
                     
