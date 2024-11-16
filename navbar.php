8959ebfe3cc2f229064c8567be88b45ff95c347b
vehzinha <101737211+vehzinha@users.noreply.github.com>
2024-11-16 18:06:45 -0300

    separando os arquivos repetidos para fazer include

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<div class="adjustable-font navbar">
+    <div class="logo">
+        <p>perdeu algo? ache aqui!</p>
+    </div>
+    <img src="img/logos/caixinha.png" alt="Logo">
+    <div class="search-bar">
+        <i class="fas fa-search"></i>
+        <input type="text" placeholder="pesquisar um item...">
+    </div>
+</div>
