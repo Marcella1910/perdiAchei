2dce5c499c298e3f36808738dd34e8236562fd9a
Marcella <101659334+Marcella1910@users.noreply.github.com>
2024-11-18 19:01:08 -0300

    atualizando a barra

--- barra-acessibilidade.php
+++ barra-acessibilidade.php
@@ -1,9 +1,12 @@
 <!-- Barra de Acessibilidade -->
-<div id="accessibility-bar">
-    <button onclick="increaseFontSize()">A+</button>
-    <button onclick="decreaseFontSize()">A-</button>
-    <button onclick="resetFontSize()">A</button>
-    <button onclick="toggleHighContrast()">Alto contraste</button>
-    <button onclick="toggleLowContrast()">Baixo contraste</button>
-    <button onclick="toggleFont()">Mudar Fonte</button>
+<div class="barra-e-botao">
+    <button id="ativa-barra"><i class="fa-solid fa-universal-access"></i></button>
+    <div id="accessibility-bar" style="display: none;">
+        <button onclick="increaseFontSize()">A+</button>
+        <button onclick="decreaseFontSize()">A-</button>
+        <button onclick="resetFontSize()">A</button>
+        <button onclick="toggleHighContrast()">Alto contraste</button>
+        <button onclick="toggleLowContrast()">Baixo contraste</button>
+        <button onclick="toggleFont()">Mudar Fonte</button>
+    </div>
 </div>
