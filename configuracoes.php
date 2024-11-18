0a1964e302e5993065fbf8babb44265ab85f5d37
Marcella <101659334+Marcella1910@users.noreply.github.com>
2024-11-18 19:01:31 -0300

    atualizando a barra

--- configuracoes.php
+++ configuracoes.php
@@ -17,9 +17,6 @@
     <!-- Inclui a Navbar -->
     <?php include 'navbar.php'; ?>
 
-    <!-- Inclui a barra de acessibilidade -->
-    <?php include 'barra-acessibilidade.php'; ?>
-
     <!-- Main Container -->
     <div class="adjustable-font container">
 
