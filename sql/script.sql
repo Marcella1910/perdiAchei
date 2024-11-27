f8a6e9040ac98696fc040fc295f976a6c726a8df
briemodd <bibiairam1@gmail.com>
2024-11-27 20:30:39 -0200

    criação do script

---
+++ sql/script.sql
@@ -0,0 +1,21 @@
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(80) NOT NULL,
+    descricao VARCHAR(280),
+    categoria VARCHAR(50),
+    status ENUM('perdido', 'encontrado') NOT NULL,
+    imagem LONGBLOB,
+    tipo_imagem VARCHAR(50),
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    usuario VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    foto_perfil VARCHAR(255),
+    token VARCHAR(255) NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
